e09d57a9571bd907bf1327367d9fa0447384b431
yoshie <b02502108@ntu.edu.com.tw>
2016-10-20 01:07:23 +0800

    new Rust repository with initial Rust file

---
+++ rust-docs/share/doc/rust/html/alloc/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["arc","Threadsafe reference-counted boxes (the `Arc<T>` type)."],["boxed","A pointer type for heap allocation."],["heap",""],["oom",""],["raw_vec",""],["rc","Unsynchronized reference-counted boxes (the `Rc<T>` type) which are usable only within a single thread."]]});
